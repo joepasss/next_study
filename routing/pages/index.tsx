434ea4b4ae7afa26186b2bf9f06f69daf8d8b523
Miller <farwes1180@gmail.com>
2022-11-30 09:05:31 +0900

    static route

---
+++ routing/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>The Home Page</h1>
+      <a href="/about">ABOUT</a>
+      <br />
+      <a href="/portfolio">PORTFOLIO</a>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ routing/pages/portfolio/list.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const List = () => {
+  return (
+    <div>
+      <h1>The List Page</h1>
+      <a href="/">HOME</a>
+      <br />
+      <a href="/portfolio">PORTFOLIO</a>
+    </div>
+  );
+};
+
+export default List;
