c6492494e10236d53485de419141120b27db56db
Miller <farwes1180@gmail.com>
2022-12-03 07:33:02 +0900

    navigating programming method

--- routing/pages/clients/[id]/index.tsx
+++ routing/pages/clients/[id]/index.tsx
@@ -1,9 +1,22 @@
+import { useRouter } from "next/router";
 import React from "react";
 
 const ClientProjectsPage = () => {
+  const router = useRouter();
+
+  const loadProjectHandler = () => {
+    // load data
+    // router.push("/clients/max/projectA");
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: { id: "max", clientprojectid: "projecta" },
+    });
+  };
+
   return (
     <div>
       <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 };
