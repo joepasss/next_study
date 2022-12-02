56cd4fb3acea6c78245b0063ece4b5f2cd865485
Miller <farwes1180@gmail.com>
2022-12-03 07:09:46 +0900

    navigte to dynamic route

--- routing/pages/clients/index.tsx
+++ routing/pages/clients/index.tsx
@@ -1,9 +1,23 @@
+import Link from "next/link";
 import React from "react";
 
 const ClientsPage = () => {
+  const clients = [
+    { id: "max", name: "Maximilian" },
+    { id: "manu", name: "Manuel" },
+  ];
+
   return (
     <div>
       <h1>The Clients Page</h1>
+
+      <ul>
+        {clients.map((item, index) => (
+          <li key={`clients_link--${index}`}>
+            <Link href={`/clients/${item.id}`}>{item.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
