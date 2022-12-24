e3cb585e898af07395750264bcc62d9c6e19d2b0
Miller <farwes1180@gmail.com>
2022-12-25 03:55:11 +0900

    getServerSideProps

---
+++ ssr_example/pages/user-profile.tsx
@@ -0,0 +1,33 @@
+import {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  GetServerSidePropsResult,
+  NextPage,
+} from "next";
+import React from "react";
+
+interface Props {
+  username: string;
+}
+
+const UserProfilePage: NextPage<Props> = (props) => {
+  const { username } = props;
+
+  return (
+    <div>
+      <h1>{username}</h1>
+    </div>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async (
+  context: GetServerSidePropsContext
+): Promise<GetServerSidePropsResult<Props>> => {
+  return {
+    props: {
+      username: "joe",
+    },
+  };
+};
+
+export default UserProfilePage;
