ab22ec5a71bf92f36971d857508a103d1e7a62d9
Miller <farwes1180@gmail.com>
2022-12-30 11:20:24 +0900

    get all events page

--- events-app/pages/events/index.tsx
+++ events-app/pages/events/index.tsx
@@ -1,17 +1,31 @@
+import { GetStaticProps, GetStaticPropsResult, NextPage } from "next";
 import React, { Fragment } from "react";
 import EventList from "../../components/events/EventList";
 import EventsSearch from "../../components/events/EventsSearch";
-import { getAllEvents } from "../../dummy-data";
+import { DummyEventsInterface, getAllEvents } from "../../helpers/api-util";
 
-const AllEventsPage = () => {
-  const events = getAllEvents();
-
+const AllEventsPage: NextPage<{ events: DummyEventsInterface[] }> = ({
+  events,
+}) => {
   return (
     <Fragment>
       <EventsSearch />
       <EventList items={events} />
     </Fragment>
   );
+};
+
+export const getStaticProps: GetStaticProps = async (): Promise<
+  GetStaticPropsResult<{ events: DummyEventsInterface[] }>
+> => {
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events,
+    },
+    revalidate: 60,
+  };
 };
 
 export default AllEventsPage;
