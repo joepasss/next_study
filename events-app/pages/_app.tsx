f7c177f7600b39d090646429316c8fed1687c8a6
Miller <farwes1180@gmail.com>
2023-01-02 04:41:25 +0900

    head overwritten

--- events-app/pages/_app.tsx
+++ events-app/pages/_app.tsx
@@ -1,10 +1,19 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import Layout from "../components/layout/Layout";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Head>
+        <meta
+          name="description"
+          content="Find a lot of great events that allow you to evolve"
+        />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <title>All Events</title>
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
